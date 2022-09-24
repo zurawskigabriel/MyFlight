66689ac2346f61d06425557911ca16492d3cd97b
zurawskigabriel <gabrielzds03@gmail.com>
2022-09-24 15:08:38 -0300

    rgerg

--- GerenciadorCias.java
+++ GerenciadorCias.java
@@ -2,4 +2,22 @@ import java.util.ArrayList;
 
 public class GerenciadorCias {
     private ArrayList<CiaAerea> empresas;
+
+    public void addcias() {
+
+    }
+
+    public CiaAerea listarcias() {
+        return;
+    }
+
+    public CiaAerea buscarcias() {
+        return;
+    }
+
+    public CiaAerea buscarnome() {
+        
+    }
+
 }
+
--- GerenciadorVoos.java
+++ GerenciadorVoos.java
@@ -2,4 +2,16 @@ import java.util.ArrayList;
 
 public class GerenciadorVoos {
     private ArrayList<Voo> voos;
+
+    public void addvoos() {
+
+    }
+
+    public Voo listarvoos() {
+        return;
+    }
+
+    public Voo buscarvoos() {
+        return;
+    }
 }
